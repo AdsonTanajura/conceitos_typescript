93a5fade7992819180f1cb3dec09e082d5fb3d02
Adson Tanajura <adson.tanajura@gmail.com>
2023-05-26 15:51:30 -0300

    Typing objects and vectors

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,17 @@
 import { Response, Request } from "express";
+import createUser from "./services/CreateUser";
 
 export function heloWord(request: Request, response: Response) {
+    const user = createUser({
+        email: 'adson.tanajura@gmail.com',
+        password: '1235',
+        techs: [
+            'React.js', 
+            'React Native', 
+            'TypeScript',
+            { title: 'JavaScript', experience: 100 },
+        ]
+    });
+
     return response.json({ message: 'Helo, World' });
 }
